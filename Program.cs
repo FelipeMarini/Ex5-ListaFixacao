f8d2b31547cce6b9348978edbd929b8ab85fa2dd
Felipe Marini <felipemarini71188@gmail.com>
2020-11-25 11:16:05 -0300

    Algorithm not finished

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ex5_ListaFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numeros = new int [10];
+
+            Console.WriteLine("Please type a number from 1 to 10 to know its multiplication table:\n ");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 10; i++)
+            {    
+                
+
+                
+                
+                
+                for (int n = 0; n < 10 ; i++)
+                {
+                    
+                
+
+
+                }
+
+            }
+            
+        
+        }          
+    }
+}
+    
